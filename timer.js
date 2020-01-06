93401a5d1f9e90bf19523b25f277e0bc038fdc6f
Krzysztof Czepiec <kontakt@krzysztofczepiec.pl>
2020-01-07 00:50:40 +0100

    update count function

--- timer.js
+++ timer.js
@@ -1,51 +1,109 @@
+// let hours = 0;
+// let minutes = 0;
+// let seconds = 0;
+// let hoursDiv = document.querySelector('#hours');
+// let minutesDiv = document.querySelector('#minutes');
+// let secondsDiv = document.querySelector('#seconds');
+// let startBtn = document.querySelector('.startCount');
+
+// hoursDiv.innerHTML = hours
+// minutesDiv.innerHTML = minutes
+// secondsDiv.innerHTML = seconds
+
+// const count = () => {
+//   hoursDiv.innerHTML = hours
+//   minutesDiv.innerHTML = minutes
+//   secondsDiv.innerHTML = seconds
+  
+//   seconds++
+  
+//   if (secondsDiv.innerHTML > 58) {
+//     minutes++
+//     seconds = 0
+//   } else if (minutesDiv.innerHTML > 58) {
+//     hours++
+//     minutes = 0
+//   }
+// };
+
+// let timer;
+
+// const startCount = () => {
+//   if(!timer) {
+//     timer = setInterval(count, 1 * 1000)
+//     startBtn.innerHTML = "Zatrzymaj"
+//     startBtn.style.backgroundColor = "#eb3b5a"
+//   } else {
+//     window.clearInterval(timer);
+//     timer = null
+//     startBtn.innerHTML = "Wznów"
+//     startBtn.style.backgroundColor = "#26de81"
+//   }
+// }
+
+// const clearCount = () => {
+//   hours = 0
+//   minutes = 0
+//   seconds = 0
+//   hoursDiv.innerHTML = hours
+//   minutesDiv.innerHTML = minutes
+//   secondsDiv.innerHTML = seconds
+// }
+
 let hours = 0;
 let minutes = 0;
 let seconds = 0;
-let hoursDiv = document.querySelector('#hours');
-let minutesDiv = document.querySelector('#minutes');
-let secondsDiv = document.querySelector('#seconds');
-let startBtn = document.querySelector('.startCount');
 
-hoursDiv.innerHTML = hours
-minutesDiv.innerHTML = minutes
-secondsDiv.innerHTML = seconds
+let container = document.querySelector('#timer');
+let startBtn = document.querySelector('#startCount');
+
+let timer = 0;
 
 const count = () => {
-  hoursDiv.innerHTML = hours
-  minutesDiv.innerHTML = minutes
-  secondsDiv.innerHTML = seconds
-  
-  seconds++
-  
-  if (secondsDiv.innerHTML > 58) {
-    minutes++
-    seconds = 0
-  } else if (minutesDiv.innerHTML > 58) {
-    hours++
-    minutes = 0
+  seconds++ ;
+  localStorage.setItem('seconds', seconds)
+  if(seconds >= 60) {
+    minutes++;
+    seconds = 0;
+    localStorage.setItem('minutes', minutes)
+  } else if (minutes >= 60) {
+    hours++;
+    minutes = 0;
+    localStorage.setItem('hours', hours)
   }
-};
-
-let timer;
+  
+  let template =
+	'<div id="hours">'+ hours +' h</div>' +
+  '<div id="minutes">'+ minutes +' m</div>' +
+  '<div id="seconds">'+ seconds +' s</div>';
+  
+  saferInnerHTML(container, template);
+}
 
 const startCount = () => {
-  if(!timer) {
+   if(!timer) {
     timer = setInterval(count, 1 * 1000)
-    startBtn.innerHTML = "Zatrzymaj"
-    startBtn.style.backgroundColor = "#eb3b5a"
+    startBtn.innerHTML = 'Zatrzymaj'
+    // console.log('Start')
   } else {
     window.clearInterval(timer);
     timer = null
-    startBtn.innerHTML = "Wznów"
-    startBtn.style.backgroundColor = "#26de81"
+    startBtn.innerHTML = 'Wznów'
+    // console.log('Pause')
   }
-}
+};
 
 const clearCount = () => {
   hours = 0
   minutes = 0
   seconds = 0
-  hoursDiv.innerHTML = hours
-  minutesDiv.innerHTML = minutes
-  secondsDiv.innerHTML = seconds
+  
+  let template =
+	'<div id="hours">'+ hours +' h</div>' +
+  '<div id="minutes">'+ minutes +' m</div>' +
+  '<div id="seconds">'+ seconds +' s</div>';
+  
+  saferInnerHTML(container, template);
+
 }
+
